326896b63d3ef134788de042291ee9c0a87df90e
github username <github email address>
2020-07-09 18:04:22 -0400

    Update post request and patch request  logic in books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,11 +21,11 @@ class BooksController < ApplicationController
 
     post '/books' do
         #binding.pry
-        @book = Book.create(book_title: params[:book_title], author: params[:author], book_genre: params[:book_genre], number_of_pages: params[:number_of_pages], start: params[:start], end: params[:end], time_one: params[:time_one], time_two: params[:time_two], read: params[:read])
-        if @book 
+        book = Book.create(book_title: params["Book Title"], author: params["Author"], book_genre: params["Book Genre"], number_of_pages: params["Number of Pages"], start: params["start"], end: params["end"], time_one: params["time_1"], time_two: params["time_2"], read: params["read"])
+        if book 
         # erb :'books/edit'
         #binding.pry
-        redirect to "/books/#{@book.id}"
+        redirect to "/books/#{book.id}"
     else
        redirect '/books/new' 
       end
@@ -46,15 +46,15 @@ class BooksController < ApplicationController
      end
 
      patch '/books/:id' do
-        @book = Book.find_by_id(params[:id])
-        book.update(book_title: params[:book_title], author: params[:author], book_genre: params[:book_genre], number_of_pages: params[:number_of_pages], start: params[:start], end: params[:end], time_one: params[:time_one], time_two: params[:time_two], read: params[:read])
-        redirect "/books/#{@book.id}"
+        book = Book.find_by_id(params[:id])
+        book.update(book_title: params["Book Title"], author: params["Author"], book_genre: params["Book Genre"], number_of_pages: params["Number of Pages"], start: params["start"], end: params["end"], time_one: params["time_1"], time_two: params["time_2"], read: params["read"])
+        redirect "/books/#{book.id}"
       end
 
       delete '/books/:id/delete' do
-        @book = Book.find_by_id(params[:id])
+        book = Book.find_by_id(params[:id])
         book.destroy
-        redirect "/books"
+        redirect '/books'
 
       end
 end
