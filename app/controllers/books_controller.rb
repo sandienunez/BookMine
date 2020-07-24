28846f18645dda41d63c187de43b8f781f56d7de
github username <github email address>
2020-07-24 12:31:52 -0400

    Refactor routes in books controller to make sure that user that is not logged in does not gain access

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,32 +3,28 @@ class BooksController < ApplicationController
 #<---CREATE ---> get '/books/new' = called NEW ACTION, renders new view
 
   get '/books/new' do
-    if logged_in?
+      redirect_if_not_logged_in
       erb :'books/new'
-    else 
-      redirect '/'
-    end
   end                              
 #<---CREATE ---> post '/books' = called create action, handles form data, then redirects 
 
   post '/books' do
-    if logged_in?
-      @user = current_user #making connection with book and user class, fetching user instance for current user 
-      @book = Book.create(book_title: params["Book Title"], author: params["Author"], book_genre: params["Book Genre"], number_of_pages: params["Number of Pages"], start: params["start"], end: params["end"], time_one: params["time_1"], time_two: params["time_2"], read: params["read"])
-      @book.user = @user
-      @book.save
+    redirect_if_not_logged_in
+    @user = current_user #making connection with book and user class, fetching user instance for current user 
+    @book = Book.create(book_title: params["Book Title"], author: params["Author"], book_genre: params["Book Genre"], number_of_pages: params["Number of Pages"], start: params["start"], end: params["end"], time_one: params["time_1"], time_two: params["time_2"], read: params["read"])
+    @book.user = @user
+    @book.save
       if @book.save
         redirect "/books/#{@book.id}"
       else
         redirect '/books/new'
       end 
-    end 
   end
 
 #<---READ ---> get '/books' do = called index action, renders an index view 
 
   get '/books' do #index route 
-    if logged_in?
+    if logged_in? 
       @user = current_user
       erb :'books/index'
     else 
@@ -51,21 +47,20 @@ class BooksController < ApplicationController
     end 
   end  
 
+
 #<---UPDATE --->  get '/books/:id/edit' = called edit action, renders edit view
   get '/books/:id/edit' do
-    if logged_in?
+    if redirect_if_not_logged_in
       @user = current_user
       @book = Book.find_by_id(params[:id])
       erb :'books/edit'
-    if authorized_to_edit?(@book)
-      erb :'books/edit'
-    else
-      flash[:error] = "Hey! Genie says you're not authorized to edit this book! So VAMOOSE! Yeah you get outta here!"
-      redirect '/books'
-    end 
-    else 
-      redirect '/login'
-    end 
+        if authorized_to_edit?(@book)
+          erb :'books/edit'
+        else
+          flash[:error] = "Hey! Genie says you're not authorized to edit this book! So VAMOOSE! Yeah you get outta here!"
+          redirect '/books'
+        end 
+    end
   end
 
 #<---UPDATE ---> patch '/books/:id' do = called update action, handles form data submission, then redirects 
@@ -84,6 +79,7 @@ class BooksController < ApplicationController
 #<---DELETE ---> delete '/books/:id' do = called delete action, deletes resource, then redirects --> triggered by button in the show and or index view
 
   delete '/books/:id' do
+      redirect_if_not_logged_in
       book = Book.find_by_id(params[:id])
       if authorized_to_edit?(book)
         book.destroy 
